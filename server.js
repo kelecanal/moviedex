c25f42251b7bbd50bd223b68e7e02d775ad7b6a0
Kareine Elecanal <kaelecanal@gmail.com>
2021-01-13 09:41:31 -0800

    final push

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.use(cors());
 app.use(helmet());
 
 app.use(function validateBearerToken(res, req, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
   console.log("validate bearer token middleware");
   next();
 });
