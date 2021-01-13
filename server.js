01aab855d704946140eccd369e7666b88d8c1480
Kareine Elecanal <kaelecanal@gmail.com>
2021-01-13 09:13:11 -0800

    updated server.js file

--- server.js
+++ server.js
@@ -5,8 +5,45 @@ const cors = require("cors");
 const helmet = require("helmet");
 const MOVIES = require("./movie-data-sm.json");
 
+console.log(process.env.API_TOKEN);
+
 const app = express();
 
-app.use(morgan(dev));
+app.use(morgan("dev"));
 app.use(cors());
 app.use(helmet());
+
+app.use(function validateBearerToken(res, req, next) {
+  console.log("validate bearer token middleware");
+  next();
+});
+
+app.get("/movie", function handleMovieGet(req, res) {
+  let response = MOVIES;
+
+  if (req.query.genre) {
+    response = response.filter((movie) =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  if (req.query.country) {
+    response = response.filter((movie) =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(
+      (movie) => Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
+
+  res.json(response);
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
