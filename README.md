5af614fb193214855fbdb8f15a56a7b0636001c4
Kareine Elecanal <kaelecanal@gmail.com>
2021-01-13 08:50:16 -0800

    README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Moviedex
+
+### User can search for movies by _genre_, _country_, or _avg_vote_.
+
+- API responds with array of full movie entries
+
+#### Created using Express
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
 const MOVIES = require("./movie-data-sm.json");
 
-const app;
+const app = express();
+
+app.use(morgan(dev));
+app.use(cors());
+app.use(helmet());
